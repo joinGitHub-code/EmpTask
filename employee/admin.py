de5bd33a37eb9f10f389f75c5fa6bcef345a16e7
joinGitHub-code <74759502+joinGitHub-code@users.noreply.github.com>
2021-02-13 20:36:15 +0530

    Update admin.py

--- employee/admin.py
+++ employee/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import *
 from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Task)
-
+#admin.site.register(Task)
+@admin.register(Task)
 class TaskAdmin(ImportExportModelAdmin):
     pass
