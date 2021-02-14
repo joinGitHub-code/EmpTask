25b42670b06965fd9cf8d98c2bf1e495d4c9e265
joinGitHub-code <74759502+joinGitHub-code@users.noreply.github.com>
2021-02-14 11:13:29 +0530

    Update models.py

--- employee/models.py
+++ employee/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -8,3 +10,7 @@ class Task(models.Model):
     time=models.DateTimeField(auto_now=True)#,auto_now_add=True
     comments=models.CharField(max_length=225, blank=True, null=True)
 
+@receiver(post_save,sender=Task)
+def saveTask(sender,instance,**kwargs):
+    #send_notification(instance)    
+    print("Task Saved Successfully")
